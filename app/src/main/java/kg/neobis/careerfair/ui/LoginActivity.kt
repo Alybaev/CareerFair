910b185897bcca7f3caaaeffe00f76b2789402ff
Dosya <alybaev_d@auca.kg>
2018-08-31 00:11:07 +0600

    first commit

---
+++ app/src/main/java/kg/neobis/careerfair/ui/LoginActivity.kt
@@ -0,0 +1,13 @@
+package kg.neobis.careerfair.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kg.neobis.careerfair.R
+
+class LoginActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
