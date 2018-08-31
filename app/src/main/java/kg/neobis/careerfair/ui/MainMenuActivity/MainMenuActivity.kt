e2071af37db965b92e69e7a964add72ec3ff0f7d
Dosya <alybaev_d@auca.kg>
2018-08-31 14:02:42 +0600

    Little bit improve UI of main menu

--- app/src/main/java/kg/neobis/careerfair/ui/MainMenuActivity/MainMenuActivity.kt
+++ app/src/main/java/kg/neobis/careerfair/ui/MainMenuActivity/MainMenuActivity.kt
@@ -2,12 +2,33 @@ package kg.neobis.careerfair.ui.MainMenuActivity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.text.Html
 import kg.neobis.careerfair.R
+import android.text.Spannable
+import android.text.style.ForegroundColorSpan
+import android.text.SpannableString
+import kotlinx.android.synthetic.main.activity_main_menu.*
+import android.os.Build
+import android.text.Spanned
+
+
+
 
 class MainMenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        about_career_fair.text = fromHtml(getString(R.string.about_career_fair_text))
+    }
+
+    fun fromHtml(html: String): Spanned {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY)
+        } else {
+            Html.fromHtml(html)
+        }
     }
 }
