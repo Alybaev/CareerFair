dba0cc1dc061ff09d0e99c959783d41c0eed1fee
Dosya <alybaev_d@auca.kg>
2018-08-31 12:13:45 +0600

    Finished UI part of login activity

--- app/src/main/java/kg/neobis/careerfair/ui/LoginActivity.kt
+++ app/src/main/java/kg/neobis/careerfair/ui/LoginActivity.kt
@@ -1,13 +1,31 @@
 package kg.neobis.careerfair.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import kg.neobis.careerfair.R
+import kg.neobis.careerfair.ui.MainMenuActivity.MainMenuActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        init()
+    }
+
+    private fun init() {
+        initListeners()
+    }
+
+    private fun initListeners() {
+        login_button.setOnClickListener{
+
+            startActivity( Intent(this,MainMenuActivity :: class.java))
+
+
+        }
     }
 }
---
+++ app/src/main/java/kg/neobis/careerfair/ui/MainMenuActivity/MainMenuActivity.kt
@@ -0,0 +1,13 @@
+package kg.neobis.careerfair.ui.MainMenuActivity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kg.neobis.careerfair.R
+
+class MainMenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
