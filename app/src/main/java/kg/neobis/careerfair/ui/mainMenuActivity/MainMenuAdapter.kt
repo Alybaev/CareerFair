544c4c9b0a0c924629ac5054a6d90efadc009cc2
Dosya <alybaev_d@auca.kg>
2018-09-04 16:24:54 +0600

    Done with ui of main menu

--- app/src/main/java/kg/neobis/careerfair/ui/mainMenuActivity/MainMenuAdapter.kt
+++ app/src/main/java/kg/neobis/careerfair/ui/mainMenuActivity/MainMenuAdapter.kt
@@ -1,6 +1,7 @@
 package kg.neobis.careerfair.ui.mainMenuActivity
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
@@ -8,13 +9,17 @@ import android.view.ViewGroup
 import kg.neobis.careerfair.R
 import kotlinx.android.synthetic.main.item_category.view.*
 import android.widget.LinearLayout
+import android.widget.RelativeLayout
 
 
 class MainMenuAdapter(var listener: Listener, var namesOfCategories: Array<String>) : RecyclerView.Adapter<MainMenuAdapter.MViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MViewHolder {
+
+
         return MViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_category, parent, false))
     }
+
 
     override fun getItemCount(): Int {
         return namesOfCategories.size
@@ -22,34 +27,42 @@ class MainMenuAdapter(var listener: Listener, var namesOfCategories: Array<Strin
 
     override fun onBindViewHolder(holder: MViewHolder, position: Int) {
         holder.categoryTextView.text = namesOfCategories[position]
-        val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-        if (position % 2 != 0) {
+        if (position % 2 == 0) {
+            holder.shadowView2.visibility = View.VISIBLE
+            holder.frameForCategory2.visibility = View.VISIBLE
+            holder.yellowLineImage2.visibility = View.VISIBLE
 
+            holder.shadowView.visibility = View.INVISIBLE
+            holder.frameForCategory.visibility = View.INVISIBLE
+            holder.yellowLineImage.visibility = View.INVISIBLE
+
+        }else {
+            holder.shadowView2.visibility = View.INVISIBLE
+            holder.frameForCategory2.visibility = View.INVISIBLE
             holder.yellowLineImage2.visibility = View.INVISIBLE
-            params.gravity =  Gravity.END
 
-        }else{
-
-            holder.yellowLineImage1.visibility = View.INVISIBLE
-            params.gravity=  Gravity.START
-
-
+            holder.shadowView.visibility = View.VISIBLE
+            holder.frameForCategory.visibility = View.VISIBLE
+            holder.yellowLineImage.visibility = View.VISIBLE
         }
-        holder.itemCategory.layoutParams = params
         holder.frameForCategory.setOnClickListener{
-
             listener.onItemSelectedAt(position)
-
+        }
+        holder.frameForCategory2.setOnClickListener{
+            listener.onItemSelectedAt(position)
         }
 
     }
 
     inner class MViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        var shadowView = view.shadow_view
-        var itemCategory = view.item_category
         var categoryTextView = view.category_text
+
+        var shadowView = view.shadow_view
         var frameForCategory = view.frame_for_category
-        var yellowLineImage1 = view.yellow_line_image1
+        var yellowLineImage = view.yellow_line_image
+
+        var shadowView2 = view.shadow_view2
+        var frameForCategory2 = view.frame_for_category2
         var yellowLineImage2 = view.yellow_line_image2
 
     }
